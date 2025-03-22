35801120a1b829755816d038b0393b937e77ba0a
GopalYad <yadavgopal6675@gmail.com>
2025-03-22 17:47:42 +0530

    hash password before auth

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcryptjs';
 const UserSchema = new mongoose.Schema(
     {
       name: {
@@ -47,7 +47,22 @@ const UserSchema = new mongoose.Schema(
       timestamps: true
     }
   );
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema)
 
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+      return next();
+    }
+  
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+  
+  // Method to compare passwords
+  UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
 
 export default User;
