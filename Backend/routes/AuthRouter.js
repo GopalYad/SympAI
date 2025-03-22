2a392fa6877ab42d7ced3fab624b3f938fa79498
GopalYad <yadavgopal6675@gmail.com>
2025-03-22 16:33:03 +0530

    configure dependenices

--- Backend/routes/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { signupValidation } from '../middleware/AuthValidate.js'
-import { signup } from '../controllers/AuthController.js'
-const router = express.Router()
-router.post('/login',(req,res)=>{
-    res.send('login succesful')
-})
-router.post('/signup',signupValidation,signup)
-export default router
